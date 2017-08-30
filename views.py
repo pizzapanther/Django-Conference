c043b6148ec30046736e3a93ce9dcbc22e51f8b5
Paul Bailey <paul.m.bailey@gmail.com>
2017-08-30 00:38:40 +0000

    setup

--- views.py
+++ views.py
@@ -1,5 +1,36 @@
-
 from django import http
+from django.views.decorators.cache import cache_page
+from django.template.response import TemplateResponse
+from django.templatetags.static import static
 
+from pytx.files import JS, CSS, FONTS, IMAGES, tpl_files
+from pytx.release import RELEASE, DEV
+
+def site_context (context):
+  context['site'] = {
+    'name': 'PyTexas'
+  }
+  
+  tpls = tpl_files()
+  
+  context['debug'] = DEV
+  context['release'] = RELEASE
+  context['files'] = {
+    'js': JS,
+    'css': CSS,
+    'fonts': FONTS,
+    'images': IMAGES,
+    'templates': tpls,
+  }
+  
+  return context
+  
+@cache_page(60 * 5, key_prefix=RELEASE)
+def favicon (request):
+  return http.HttpResponseRedirect(static('favicon.ico'))
+  
+@cache_page(60 * 5, key_prefix=RELEASE)
 def frontend (request):
-  return http.HttpResponse('Coming Soon', content_type="text/plain")
+  context = {}
+  return TemplateResponse(request, 'frontend.html', site_context(context))
+  
