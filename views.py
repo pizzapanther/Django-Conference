fd5ca842f749d9cf7dcb6a90b9f63454c2e7a0bc
Paul Bailey <paul.m.bailey@gmail.com>
2017-08-31 19:47:21 +0000

    service worker

--- views.py
+++ views.py
@@ -1,11 +1,11 @@
 from django import http
+from django.conf import settings
 from django.views.decorators.cache import cache_page
 from django.template.response import TemplateResponse
 from django.templatetags.static import static
 
 from pytx.files import JS, CSS, FONTS, IMAGES, MD, tpl_files
 from pytx.release import RELEASE, DEV
-
 
 def site_context(context):
   context['site'] = {'name': 'PyTexas'}
@@ -14,6 +14,7 @@ def site_context(context):
 
   context['debug'] = DEV
   context['release'] = RELEASE
+  context['conf'] = settings.CURRENT_CONF
   context['files'] = {
       'js': JS,
       'css': CSS,
@@ -34,8 +35,7 @@ def favicon(request):
 @cache_page(60 * 5, key_prefix=RELEASE)
 def frontend(request):
   if request.path == '/':
-    #todo: use current conference
-    return http.HttpResponseRedirect("/2017/")
+    return http.HttpResponseRedirect("/{}/".format(settings.CURRENT_CONF))
 
   context = {}
   return TemplateResponse(request, 'frontend.html', site_context(context))
