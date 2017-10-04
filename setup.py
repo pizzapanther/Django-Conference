1ea0aab7949475f3971ba772ee88d937585e0999
Paul Bailey <paul.m.bailey@gmail.com>
2017-10-04 00:11:33 +0000

    package setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='conference',
+  version='1.0',
+  description='Django conference app',
+  url='https://github.com/pizzapanther/Django-Conference',
+  author='Paul Bailey',
+  author_email='paul@neutron.academy',
+  license='MIT',
+  packages=['conference'],
+  zip_safe=True
+)
